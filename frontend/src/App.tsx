30984772cb489271c1014b2b8e718f37999052ce
randiranirman <randiranirman20011210@gmail.com>
2025-11-01 21:40:57 +0530

    test commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,12 +6,15 @@ export default function App() {
   const { user } = useUser()
   const { openSignIn } = useClerk()
 
+  console.log("user", user)
+
   return (
     /** here is the button to logged in want to impliment more **/
     <div>
+
       <div className="flex min-h-svh flex-col items-center justify-center gap-4">
         {
-          !user ? (<Button onClick={() =>openSignIn}>Login</Button>) : (<UserButton />)
+          !user ? (<Button onClick={() =>openSignIn()}>Login</Button>) : (<UserButton />)
         }
         
       </div>
